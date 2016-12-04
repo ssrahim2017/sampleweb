9d1e3110e7bae59f3642e6c706713816eddc34b4
ssrahim2017 <rahimbazi1981@gmail.com>
2016-12-04 19:04:02 +0530

    added tab functionality

---
+++ app/script/cart.js
@@ -0,0 +1,38 @@
+function handleTabClick(tab){
+//show clicked tab 
+	//hide other tabs 
+var men = document.getElementById('m'); 
+var women = document.getElementById('w'); 
+var boy = document.getElementById('b'); 
+var girl = document.getElementById('g'); 
+
+//'men' 'women' 'boy' 'girl' 
+if(tab === 'men'){ 		
+console.log('men clicked');
+men.className = "";
+women.className = "hide";
+boy.className = "hide";
+girl.className = "hide"; 	
+			} 
+else if(tab === 'women'){
+console.log('women clicked');
+men.className = "hide";
+women.className = "";
+boy.className = "hide";
+girl.className = "hide"; 	
+	} 
+else if(tab === 'boy'){ 		
+console.log('boy clicked');
+men.className = "hide";
+women.className = "hide";
+boy.className = "";
+girl.className = "hide"; 	
+		} 
+else if(tab === 'girl'){ 
+console.log ('girl clicked')
+men.className = "hide";
+women.className = "hide";
+boy.className = "hide";
+girl.className = "";		
+}
+	}
